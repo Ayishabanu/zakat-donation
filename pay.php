65daf3d783852e0d95867647a2c77d2de67739a8
Ayishabanu <71723925+Ayishabanu@users.noreply.github.com>
2021-05-06 20:26:18 -1000

    Update pay.php

--- pay.php
+++ pay.php
@@ -1,7 +1,7 @@
 <?php
 require('./instamojo.php');
-const API_KEY ="test_c33f81e1cc213f1180dea94e4ef";
-const AUTH_TOKEN = "test_1c5f40f237532eb77a4ea1b8540";
+const API_KEY ="Your_API_key";
+const AUTH_TOKEN = "Your_auth_token";
 
 
 if(isset($_POST['purpose']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['amount']))
