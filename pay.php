16429676cbd670e46eef263caf8bc2e47afbdac5
Ayishabanu <71723925+Ayishabanu@users.noreply.github.com>
2021-05-06 20:17:09 -1000

    Add files via upload

---
+++ pay.php
@@ -0,0 +1,26 @@
+<?php
+require('./instamojo.php');
+const API_KEY ="test_c33f81e1cc213f1180dea94e4ef";
+const AUTH_TOKEN = "test_1c5f40f237532eb77a4ea1b8540";
+
+
+if(isset($_POST['purpose']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['amount']))
+{
+    $api = new Instamojo\Instamojo(API_KEY, AUTH_TOKEN,'https://test.instamojo.com/api/1.1/');
+    
+    try {
+        $response = $api->paymentRequestCreate(array(
+            "purpose" => $_POST['purpose'],
+            "buyer_name" => $_POST['name'],
+            "amount" => $_POST['amount'],
+            "send_email" => true,
+            "email" => $_POST['email'],
+            "redirect_url" => "http://ayisha-product.infinityfreeapp.com/success.html"
+            ));
+        header('Location:'. $response['longurl']);
+    }
+    catch (Exception $e) {
+        print('Error: ' . $e->getMessage());
+    }
+}
+?>
